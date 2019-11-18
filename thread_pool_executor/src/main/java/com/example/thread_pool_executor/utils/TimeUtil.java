09cd208d0b36bd34d2af7f3ace923ef326b01b09
zhangls <418194367@qq.com>
2019-11-18 17:55:40 +0800

    添加时间工具类

---
+++ thread_pool_executor/src/main/java/com/example/thread_pool_executor/utils/TimeUtil.java
@@ -0,0 +1,252 @@
+package com.example.thread_pool_executor.utils;
+
+/**
+ * <pre>项目名称：yiautos-union-service
+ * 类名称：TimeUtil
+ * 类描述：
+ * 创建人：zhangliansheng
+ * 创建时间：2019/11/18 11:44
+ */
+
+import org.joda.time.DateTime;
+
+import java.time.*;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.TemporalAdjuster;
+import java.time.temporal.TemporalAdjusters;
+import java.util.Date;
+
+public class TimeUtil {
+
+    //北京时区
+    public static ZoneId bjZone = ZoneId.of("GMT+08:00");
+    /**
+     * 日期时间格式yyyy-MM-dd HH:mm:ss
+     */
+    public static String DATE_TIME_PATTERN = "yyyy-MM-dd HH:mm:ss";
+    /**
+     * 日期时间格式yyyy-MM-dd HH:mm:ss
+     */
+    public static String DATE_TIME = "yyyy-MM-dd";
+
+    public static DateTimeFormatter df = DateTimeFormatter.ofPattern(DATE_TIME);
+
+    //获取当前时间
+    public static LocalDate inputDate = LocalDate.now();
+
+    //本周开始时间
+
+    public static LocalDateTime weekStart = date2LocalDateTime(new DateTime().minusDays(new DateTime().getDayOfWeek() - 1).withTimeAtStartOfDay().toDate());
+    //本周结束时间
+    public static TemporalAdjuster LAST_OF_WEEK =
+            TemporalAdjusters.ofDateAdjuster(localDate -> localDate.plusDays(DayOfWeek.SUNDAY.getValue() - localDate.getDayOfWeek().getValue()));
+    public static String weekEnd = df.format(inputDate.with(LAST_OF_WEEK));
+
+
+    /**
+     * Date转LocalDateTime
+     *
+     * @param date Date对象
+     * @return
+     */
+    public static LocalDateTime dateToLocalDateTime(Date date) {
+        return LocalDateTime.ofInstant(date.toInstant(), ZoneId.systemDefault());
+    }
+
+    /**
+     * LocalDateTime转换为Date
+     *
+     * @param dateTime LocalDateTime对象
+     * @return
+     */
+    public static Date localDateTimeToDate(LocalDateTime dateTime) {
+        return Date.from(dateTime.atZone(ZoneId.systemDefault()).toInstant());
+    }
+
+    /**
+     * 格式化时间-默认yyyy-MM-dd HH:mm:ss格式
+     *
+     * @param dateTime LocalDateTime对象
+     * @return
+     */
+    public static String formatDateTime(LocalDateTime dateTime) {
+        return formatDateTime(dateTime, DATE_TIME_PATTERN);
+    }
+
+    /**
+     * 按pattern格式化时间-默认yyyy-MM-dd HH:mm:ss格式
+     *
+     * @param dateTime LocalDateTime对象
+     * @param pattern  要格式化的字符串
+     * @return
+     */
+    public static String formatDateTime(LocalDateTime dateTime, String pattern) {
+        if (dateTime == null) {
+            return null;
+        }
+        if (pattern == null || pattern.isEmpty()) {
+            pattern = DATE_TIME_PATTERN;
+        }
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern(pattern);
+        return dateTime.format(formatter);
+    }
+
+    /**
+     * 获取今天的00:00:00
+     *
+     * @return
+     */
+    public static String getDayStart() {
+        return getDayStart(LocalDateTime.now());
+    }
+
+    /**
+     * 获取今天的23:59:59
+     *
+     * @return
+     */
+    public static String getDayEnd() {
+        return getDayEnd(LocalDateTime.now());
+    }
+
+    /**
+     * 获取某天的00:00:00
+     *
+     * @param dateTime
+     * @return
+     */
+    public static String getDayStart(LocalDateTime dateTime) {
+        return formatDateTime(dateTime.with(LocalTime.MIN));
+    }
+
+    /**
+     * 获取某天的23:59:59
+     *
+     * @param dateTime
+     * @return
+     */
+    public static String getDayEnd(LocalDateTime dateTime) {
+        return formatDateTime(dateTime.with(LocalTime.MAX));
+    }
+
+    /**
+     * 获取本月第一天的00:00:00
+     *
+     * @return
+     */
+    public static String getFirstDayOfMonth() {
+        return getFirstDayOfMonth(LocalDateTime.now());
+    }
+
+    /**
+     * 获取本月最后一天的23:59:59
+     *
+     * @return
+     */
+    public static String getLastDayOfMonth() {
+        return getLastDayOfMonth(LocalDateTime.now());
+    }
+
+    /**
+     * 获取某月第一天的00:00:00
+     *
+     * @param dateTime LocalDateTime对象
+     * @return
+     */
+    public static String getFirstDayOfMonth(LocalDateTime dateTime) {
+        return formatDateTime(dateTime.with(TemporalAdjusters.firstDayOfMonth()).with(LocalTime.MIN));
+    }
+
+    /**
+     * 获取某月最后一天的23:59:59
+     *
+     * @param dateTime LocalDateTime对象
+     * @return
+     */
+    public static String getLastDayOfMonth(LocalDateTime dateTime) {
+        return formatDateTime(dateTime.with(TemporalAdjusters.lastDayOfMonth()).with(LocalTime.MAX));
+    }
+
+
+    /**
+     * @return 返回当月天数
+     * @Author zhangliansheng
+     * @Description
+     * @Date 14:04 2019/11/18
+     * @Param
+     **/
+
+
+    public static int days(int year, int month) {
+        int days = 0;
+        if (month != 2) {
+            switch (month) {
+                case 1:
+                case 3:
+                case 5:
+                case 7:
+                case 8:
+                case 10:
+                case 12:
+                    days = 31;
+                    break;
+                case 4:
+                case 6:
+                case 9:
+                case 11:
+                    days = 30;
+
+            }
+        } else {
+            // 闰年
+            if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0)
+                days = 29;
+            else
+                days = 28;
+
+        }
+        return days;
+    }
+
+    /**
+     * Date转换为LocalDateTime
+     *
+     * @param date
+     */
+    public static LocalDateTime date2LocalDateTime(Date date) {
+        Instant instant = date.toInstant();//An instantaneous point on the time-line.(时间线上的一个瞬时点。)
+        LocalDateTime localDateTime = instant.atZone(bjZone).toLocalDateTime();
+
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");//This class is immutable and thread-safe.@since 1.8
+        return localDateTime;
+    }
+
+    /**
+     * LocalDateTime转换为Date
+     *
+     * @param localDateTime
+     */
+    public static Date localDateTime2Date(LocalDateTime localDateTime) {
+        ZonedDateTime zdt = localDateTime.atZone(bjZone);//Combines this date-time with a time-zone to create a  ZonedDateTime.
+        Date date = Date.from(zdt.toInstant());
+        return date;
+    }
+
+    /**
+     * 测试
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+//        System.out.println(getDayStart());
+//        System.out.println(getDayEnd());
+//        System.out.println(getFirstDayOfMonth());
+//        System.out.println(getLastDayOfMonth());
+//        System.out.println(days(LocalDateTime.now().getYear(), LocalDateTime.now().getMonth().getValue()));
+        System.out.println(TimeUtil.weekStart);
+        System.out.println(TimeUtil.weekEnd);
+        LocalDateTime localDateTime = date2LocalDateTime(new DateTime().minusDays(new DateTime().getDayOfWeek() - 1).withTimeAtStartOfDay().toDate());
+        System.out.println(getDayStart(localDateTime));
+    }
+
+}
